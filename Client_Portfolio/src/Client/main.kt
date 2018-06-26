26bc78c30a74aeb3d79a9a7006554404ceb26573
syou4 <g1444693@gmail.com>
2018-06-26 12:03:17 +0900

    Mainクラス定義

--- Client_Portfolio/src/Client/main.kt
+++ Client_Portfolio/src/Client/main.kt
@@ -5,22 +5,23 @@ import client.Inputer
 import client.View
 */
 import java.net.*
-class Main
 
-fun main(args:Array<String>) {
-/*
-val connecter : Connecter = Connecter();
-val inputer : Inputer = Inputer();
-val view : View = View();
-*/
-val host = "localhost"
-val port = 12345
-val sendData = "hoge".toByteArray()
 
-send(host, port, sendData)
+class Main{
+    fun main(args:Array<String>) {
+        /*
+        val connecter : Connecter = Connecter();
+        val inputer : Inputer = Inputer();
+        val view : View = View();
+        */
+        val host = "localhost"
+        val port = 12345
+        val sendData = "hoge".toByteArray()
 
+        send(host, port, sendData)
+
+    }
 }
-
 
 fun send(host: String, port: Int, data: ByteArray, senderPort: Int = 0): Boolean {
     var ret = false
