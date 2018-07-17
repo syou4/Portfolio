a45206c5756ac2a6063e9abefe5aca93a1e7c179
soutarou <kanikani.egg1210@gmail.com>
2018-07-17 11:36:50 +0900

    メッセージ受信機能とそれに対する応答機能の追加

--- Server_Portfolio/src/pfserver/PortfolioServer.kt
+++ Server_Portfolio/src/pfserver/PortfolioServer.kt
@@ -3,28 +3,43 @@
 import java.net.*
 
 fun main(args: Array<String>) {
-    // val xmlReader = XMLReader()
-    val socket = DatagramSocket()
-    socket.connect(Inet4Address.getByName("127.0.0.1"),12345)
-
-    val data = ByteArray(128)
+    //ポート12345で受信用ソケットを生成
+    val rcvSocket = DatagramSocket(12345)
+    //128バイトのバッファを用意
+    val rcvData = ByteArray(128)
 
     System.out.println("Receiving ...")
 
-    val pkt = DatagramPacket(data, data.size)
-    socket.receive(pkt)
+    //受信パケットを格納するバッファを生成、大きさはデータ格納用バッファと同じ
+    val rcvPkt = DatagramPacket(rcvData, rcvData.size)
+    //受信待機
+    rcvSocket.receive(rcvPkt)
 
-    val header = (0 shl 30) or (4 shl 27) or (3 shl 24) or (15 shl 16) or (4 shl 8)
-    data[0] = (header shr 24).toByte()
-    data[1] = (header shr 16).toByte()
-    data[2] = (header shr 8).toByte()
-    data[3] = (header shr 0).toByte()
+    System.out.println("Received")
 
-    pkt.length =  4 * 12
-    socket.send(pkt)
+    //受信したパケットの内容をバッファにコピー
+    rcvData.copyOf(rcvPkt.length)
+    //受信したバイト情報を文字列に変換
+    val rcvStr = String(rcvData,0,128)
 
-    System.out.println(data.copyOf(pkt.length).joinToString(
-        separator = " ",
-        transform = { b -> String.format("%02x", b) }
-    ))
+    System.out.println("rcv Message > " + rcvStr)
+
+    //受信ソケットクローズ
+    rcvSocket.close()
+    //2秒待機
+    Thread.sleep(2000)
+
+    //応答用ソケット生成
+    val sendSocket = DatagramSocket()
+    //応答文字列
+    val sendStr : String = "ACK"
+    //送信データのバイト配列
+    val sendData = sendStr.toByteArray(Charsets.UTF_8)
+    //送信パケット生成
+    val sendPkt = DatagramPacket(sendData, sendData.size,Inet4Address.getByName("localhost"),12345)
+    System.out.println("Responsing ...")
+    sendSocket.send(sendPkt)
+    System.out.println("Responsed ...")
+
+    System.out.println("Fin.")
 }
